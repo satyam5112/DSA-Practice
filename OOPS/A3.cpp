5135332a22a728d607765c083667caa7cd1093f9
Satyam Singh <singhsatyam5112@gmail.com>
2024-03-12 12:22:42 +0530

    minor changes

--- OOPS/A3.cpp
+++ OOPS/A3.cpp
@@ -1,33 +1,61 @@
-// PATTERN PRINTING.
+// // PATTERN PRINTING.
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int h=5,i,j;
+//     // cout<<"Enter the height:";
+//     // cin>>h;
+//     for(i=1;i<=h;i++)
+//     {
+//         char al='A';
+//         for(j=1;j<(2*h);j++)
+//         {
+//             if(j<i)
+//             {
+//                 cout<<" ";
+//                 cout<<" ";
+//             }
+//             if(j>=i && i+j<=10)
+//             {
+//                 cout<<al++;
+//                 cout<<" ";
+//             }
+//             if(i+j>10)
+//             {
+//                 cout<<" ";
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<"\n";
+//     } 
+//     return 0;
+// }
 #include<iostream>
 using namespace std;
-int main()
-{
-    int h=5,i,j;
-    // cout<<"Enter the height:";
-    // cin>>h;
-    for(i=1;i<=h;i++)
-    {
-        char al='A';
-        for(j=1;j<(2*h);j++)
-        {
-            if(j<i)
-            {
-                cout<<" ";
-                cout<<" ";
+
+int main() {
+    int h;
+    cout << "Enter the height: ";
+    cin >> h;
+
+    for(int i = 1; i <= h; i++) {
+        char al = 'A';
+        for(int j = 1; j < (2 * h); j++) {
+            if(j < i) {
+                cout << " ";
+                cout << " ";
             }
-            if(j>=i && i+j<=10)
-            {
-                cout<<al++;
-                cout<<" ";
+            if(j >= i && i + j <= (2 * h - 1)) {
+                cout << al++;
+                cout << " ";
             }
-            if(i+j>10)
-            {
-                cout<<" ";
-                cout<<" ";
+            if(i + j > (2 * h - 1)) {
+                cout << " ";
+                cout << " ";
             }
         }
-        cout<<"\n";
-    } 
+        cout << "\n";
+    }
     return 0;
 }
